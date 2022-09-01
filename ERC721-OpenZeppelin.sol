b00c7f2225dfb224f33175577e5b640b89df9b30
Sreekar Mouli <sreekar.mouli1998@gmail.com>
2022-09-01 21:41:20 +0530

    Added ERC721 smart contract using open zeppelin

---
+++ ERC721-OpenZeppelin.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+import "@openzeppelin/contracts@4.7.3/utils/Counters.sol";
+
+contract MyToken is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyToken", "MTK") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < 10, "No more tokens can be minted!");
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
